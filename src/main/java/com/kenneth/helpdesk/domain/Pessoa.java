5d94b6fbdd5ffb1c0bb472cb4dcab0de8c0ab3d8
StanleyKenneth <kstanley_bass@hotmail.com>
2023-05-15 19:49:55 -0300

    Anotações JPA para classes de domínio

--- src/main/java/com/kenneth/helpdesk/domain/Pessoa.java
+++ src/main/java/com/kenneth/helpdesk/domain/Pessoa.java
@@ -1,21 +1,44 @@
 package com.kenneth.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kenneth.helpdesk.domain.enums.Perfil;
 
-public abstract class Pessoa {
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public abstract class Pessoa implements Serializable {
+   	private static final long serialVersionUID = 1L;
+	
+   	@Id
+   	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	protected Integer id;
 	protected String nome;
+	
+	@Column(unique = true)
 	protected String cpf;
+	@Column(unique = true)
 	protected String email;
 	protected String senha;
+	
+	@ElementCollection(fetch = FetchType.EAGER)// Assegurando que a lista de perfis vem junto com o usuário.
+	@CollectionTable(name = "PERFIS")
 	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now(); // Pega o momento atual em que a instacia do objeto foi criada.
 
 	public Pessoa() {
