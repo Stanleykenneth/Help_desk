66ef11cd21154db7f271143df0cf4a3ed90dd3ed
StanleyKenneth <kstanley_bass@hotmail.com>
2023-05-17 15:11:15 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/kenneth/helpdesk/services/TecnicoService.java
+++ src/main/java/com/kenneth/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.kenneth.helpdesk.domain.Tecnico;
 import com.kenneth.helpdesk.repositories.TecnicoRepository;
+import com.kenneth.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não Encontrado! Id: " + id));
 	}
 }
